a0bdef7c70f7200e6a760bf95c26ff6aeb318e99
Yashita Bansal <byashita.10@gmail.com>
2026-07-30 08:59:28 +0000

    Code challenge submission for question: 392 Is Subsequence

---
+++ 392 Is Subsequence/392is-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+
+        int i = 0; // pointer for s
+        int j = 0; // pointer for t
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        if (i == s.length()) {
+         return true;
+}       else {
+         return false;
+}
+    }
+}
