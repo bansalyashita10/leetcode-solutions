c7fb7146fd47cefb72e7ebe872518b36d09b5ca2
Yashita Bansal <byashita.10@gmail.com>
2026-08-08 18:19:54 +0000

    Code challenge submission for question: 15 3Sum

---
+++ 15 3Sum/153sum.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicate values of i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == 0) {
+                    result.add(
+                        Arrays.asList(nums[i], nums[j], nums[k])
+                    );
+                    j++;
+                    k--;
+                    // Skip duplicate values of j
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                    // Skip duplicate values of k
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+                }
+                else if (sum < 0) {
+                    j++;
+                }
+                else {
+                    k--;
+                }
+            }
+        }
+        return result;
+    }
+}
