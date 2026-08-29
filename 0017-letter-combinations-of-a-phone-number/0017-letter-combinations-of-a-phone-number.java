1de9511f9c734a85750ede125d9175f0a196dcda
bansalyashita10 <yashita2578.beai24@chitkara.edu.in>
2026-08-29 21:54:15 +0530

    Time: 3 ms (32.6%), Space: 48.9 MB (19.46%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+
+        List<String> result = new ArrayList<>();
+        String[] map = {
+            "", "", "abc", "def", "ghi",
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+
+        generate(digits, 0, "", result, map);
+
+        return result;
+    }
+
+    public void generate(String digits, int index, String current,
+                         List<String> result, String[] map) {
+
+        // Base case
+        if (index == digits.length()) {
+            result.add(current);
+            return;
+        }
+
+        // Get letters corresponding to current digit
+        String letters = map[digits.charAt(index) - '0'];
+
+        // Try every possible letter
+        for (char ch : letters.toCharArray()) {
+            generate(
+                digits,
+                index + 1,
+                current + ch,
+                result,
+                map
+            );
+        }
+        
+    }
+}
