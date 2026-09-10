9d377758a05a948ab3cb5035bee1e86445179399
bansalyashita10 <yashita2578.beai24@chitkara.edu.in>
2026-09-10 14:28:15 +0530

    Time: 0 ms (100%), Space: 48.1 MB (33.48%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,19 @@
 class Solution {
-    public double myPow(double x, int n) {
-       return (double)Math.pow(x, n);
-      
-
+    public double myPow(double x, long n) {
+        if(n==0){
+            return 1;
+        }
+        if (n < 0) {
+            return 1 / myPow(x, -(long)n);
+        }
+        if(n==1){
+            return x;
+        }
+        if(n%2==0){
+            return myPow(x*x,n/2);
+        }
+        else{
+            return x*myPow(x,n-1);
+        }
     }
 }
